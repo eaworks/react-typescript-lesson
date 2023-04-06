0f5d230fe11bfc0bd41e913152566e74d25319d4
Ersin Aydın <ersinaydinworks@gmail.com>
2023-04-06 23:51:03 +0300

    boolean test

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Home from './Components/Home';
 function App() {
   return (
     <div className="App">
-      <Home name="Ersin Aydin" courseNumber={15} />
+      <Home name="Ersin Aydin" courseNumber={15} isBest={false} />
     </div>
   );
 }
--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -2,11 +2,14 @@ import React from 'react'
 type DataComing = {
     name: string;
     courseNumber: number;
+    isBest: boolean;
 }
 
 function Home(props: DataComing) {
     return (
-        <div>{props.name} Egitime hosgeldiniz : {props.courseNumber}</div>
+        <div>{props.name} Egitime hosgeldiniz : {props.courseNumber}
+            {props.isBest ? (<p>En iyi React eğitimi bu eğitimim </p>) : (<p>En iyi React eğitimi bu eğitimim değil ileride bu eğitim </p>)}
+        </div>
     )
 }
 
